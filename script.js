0617e82d05b4fe655f1c70dbcb93809cece9dd23
mys <miccelinski@gmail.com>
2018-10-04 15:46:38 +0200

    Create delegation history

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const INITIAL_FETCH_LIMIT = 500;
+let [accounts, accountHistory, delegations, dynamicGlobalProperties] = [];
+let delegationHistory;
 
 steem.api.setOptions({ url: 'https://api.steemit.com' });
 usernameSubmitted();
 
 async function usernameSubmitted(){
     let name = 'wefund';
-	let [accounts, accountHistory, delegations, dynamicGlobalProperties] = await Promise.all([
+	[accounts, accountHistory, delegations, dynamicGlobalProperties] = await Promise.all([
 		steem.api.getAccountsAsync([name]),
 		steem.api.getAccountHistoryAsync(name, -1, INITIAL_FETCH_LIMIT),
 		steem.api.getVestingDelegationsAsync(name, -1, 100),
@@ -15,7 +17,9 @@ async function usernameSubmitted(){
 	if (!accounts[0]) return
 	if (!accountHistory) return
 	if (!delegations) return
-	if (!dynamicGlobalProperties) return
+    if (!dynamicGlobalProperties) return
+
+	accountHistory = accountHistory.reverse()
 
 	let delegationsObj = {}
     delegations.forEach((item) => {
@@ -32,7 +36,61 @@ async function usernameSubmitted(){
 	console.log(accounts);
 	console.log(accountHistory);
 	console.log(delegations);
-	console.log(dynamicGlobalProperties);
+    console.log(dynamicGlobalProperties);
+
+    delegationHistory = await buildDelegationHistory(accountHistory, delegations);
+    console.log(delegationHistory);
+}
+
+async function buildDelegationHistory(accountHistory, currentDelegations){
+	let delegationHistory = [];
+
+	if (_.isEmpty(accountHistory)) return delegationHistory
+
+	const delegationKeys = Object.keys(currentDelegations)
+	const accountHistoryEnd = moment(_.head(accountHistory)[1].timestamp, moment.ISO_8601)
+	const accountHistoryStart = moment(_.last(accountHistory)[1].timestamp, moment.ISO_8601)
+
+	_.forOwn(currentDelegations, (delegation) => {
+		const { delegator, delegatee, vesting_shares, vesting_shares_sp } = delegation
+		delegationHistory[`${delegator}_${delegatee}`] = {
+		  delegator,
+		  delegatee,
+		  vestingShares: vesting_shares,
+		  steemPower: vesting_shares_sp,
+		  hasMoreData: true,
+		  // startDate might be overwritten when we encounter a txType of delegate_vesting_shares
+		  startDate: accountHistoryStart,
+		  endDate: accountHistoryEnd,
+		  transfers: []
+		}
+	})
+
+	accountHistory.forEach((tx) => {
+		const txType = tx[1].op[0]
+		const txData = tx[1].op[1]
+		if (txType === 'transfer') {
+		  const delegationKey = `${txData.to}_${txData.from}`
+		  if (delegationKeys.includes(delegationKey)) {
+			delegationHistory[delegationKey].transfers.push(tx)
+		  }
+		} else {
+		  // tx is of type TRANSACTION_TYPES.DELEGATE_VESTING_SHARES
+		  const delegationKey = `${txData.delegator}_${txData.delegatee}`
+		  // Only process current delegations, ignore the rest
+		  if (delegationKeys.includes(delegationKey)) {
+			// We found when the delegation started, so we overwrite the startDate initialized from accountHistory.
+			// This also means we have all data collected for the current delegation.
+			delegationHistory[delegationKey].startDate = moment(tx[1].timestamp, moment.ISO_8601)
+			// Read all transactions for this delegation, no more data available.
+			delegationHistory[delegationKey].hasMoreData = false
+			// remove delegation key, because we already collected all transactions from the blockchain
+			_.pull(delegationKeys, delegationKey)
+		  }
+		}
+    })
+
+	return delegationHistory
 }
 
 function unitString2Number(stringWithUnit){
